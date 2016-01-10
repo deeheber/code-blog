ce67eee942f5865c3282855b67e09a6e58e8afb8
Danielle H <daheber@gmail.com>
2016-01-10 11:33:43 -0800

    top nav js for mobile

---
+++ scripts/topNav.js
@@ -0,0 +1,24 @@
+var topNav = {};
+
+topNav.handleNav = function(){
+  $('#cross').hide();
+  $('#main-nav').hide();
+  /*** Expose the nav when clicking the hamburger ***/
+  $('#hamburger').on('click', function(){
+    $('#main-nav').slideToggle('slow', function(){
+      $('#hamburger').hide();
+      $('#cross').show();
+    });
+  });
+  /*** Hide nav when x is clicked on ***/
+  $('#cross').on('click', function(){
+    $('#main-nav').slideToggle('slow', function(){
+      $('#cross').hide();
+      $('#hamburger').show();
+    });
+  });
+};
+
+$(document).ready(function() {
+  topNav.handleNav();
+});
