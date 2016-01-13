28d608eeffec5fe0e89b628d33e25c6f6b0f5968
Danielle H <daheber@gmail.com>
2016-01-12 20:08:08 -0800

    handlebar template input. Need to fix body text.

--- scripts/project.js
+++ scripts/project.js
@@ -9,22 +9,33 @@ function Post(option){
 }
 
 Post.prototype.toHtml = function() {
-  var $newPost = $('article.template').clone();
+  var theTemplateScript  = $('#post-template').html();
+  var theTemplate = Handlebars.compile(theTemplateScript);
+
+  var context = {
+    'category': this.category,
+    'title': this.title,
+    'publishedOn': this.publishedOn,
+    'projectURL': this.projectURL,
+    'body': this.body
+  };
+
+  var theCompiledHml = theTemplate(context);
+  return theCompiledHml;
 
   /***Adding category attribute to each post for filtering***/
-  $newPost.attr('data-category', this.category);
+  //$newPost.attr('data-category', this.category);
 
   /***Pulling post data into template***/
-  $newPost.find('h2').text(this.title);
-  $newPost.find('.date').append(' on ' + this.publishedOn);
-  $newPost.find('.post_body').html(this.body);
-  $newPost.find('.external_link a').attr('href', this.projectURL);
+  //$newPost.find('h2').text(this.title);
+  //$newPost.find('.date').append(' on ' + this.publishedOn);
+  //$newPost.find('.post_body').html(this.body);
+  //$newPost.find('.external_link a').attr('href', this.projectURL);
 
-  $newPost.removeAttr('class', 'template');
+  //$newPost.removeAttr('class', 'template');
 
-  $newPost.find('time').html('Finished ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
+  //$newPost.find('time').html('Finished ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
 
-  return $newPost;
 };
 
 /***Sorts blog by newest projects first ***/
