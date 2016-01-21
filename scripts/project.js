8da8438ff7f0adb3aa92e3b040712036c6c84248
Danielle H <daheber@gmail.com>
2016-01-20 21:41:03 -0800

    IIFE added for project and projectView js files.

--- scripts/project.js
+++ scripts/project.js
@@ -1,75 +1,78 @@
-Post.all = [];
+(function(module){
+  Post.all = [];
 
-function Post(option){
-  this.title = option.title;
-  this.publishedOn = option.publishedOn;
-  this.category = option.category;
-  this.body = option.body;
-  this.projectURL = option.projectURL;
-}
+  function Post(option){
+    this.title = option.title;
+    this.publishedOn = option.publishedOn;
+    this.category = option.category;
+    this.body = option.body;
+    this.projectURL = option.projectURL;
+  }
 
-Post.prototype.toHtml = function() {
-  var theTemplateScript  = $('#post-template').html();
-  var theTemplate = Handlebars.compile(theTemplateScript);
+  Post.prototype.toHtml = function() {
+    var theTemplateScript  = $('#post-template').html();
+    var theTemplate = Handlebars.compile(theTemplateScript);
 
-  /*** Calculation for how long ago a post was created ***/
-  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
-  this.dateInfo = this.publishedOn ? 'Published ' + this.daysAgo + ' days ago' : '(draft)';
+    /*** Calculation for how long ago a post was created ***/
+    this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+    this.dateInfo = this.publishedOn ? 'Published ' + this.daysAgo + ' days ago' : '(draft)';
 
-  var context = {
-    'category': this.category,
-    'title': this.title,
-    'publishedOn': this.publishedOn,
-    'projectURL': this.projectURL,
-    'body': this.body,
-    'dateInfo': this.dateInfo
+    var context = {
+      'category': this.category,
+      'title': this.title,
+      'publishedOn': this.publishedOn,
+      'projectURL': this.projectURL,
+      'body': this.body,
+      'dateInfo': this.dateInfo
+    };
+
+    var theCompiledHml = theTemplate(context);
+
+    return theCompiledHml;
   };
 
-  var theCompiledHml = theTemplate(context);
+  Post.loadAll = function(sourceData){
+    /***Sorts blog by newest projects first ***/
+    sourceData.sort(function(a,b) {
+      return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+    });
 
-  return theCompiledHml;
-};
-
-Post.loadAll = function(sourceData){
-  /***Sorts blog by newest projects first ***/
-  sourceData.sort(function(a,b) {
-    return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
-  });
-
-  /*** Push new post to Posts array and write to the html ***/
-  sourceData.forEach(function(el){
-    Post.all.push(new Post(el));
-  });
-};
+    /*** Push new post to Posts array and write to the html ***/
+    sourceData.forEach(function(el){
+      Post.all.push(new Post(el));
+    });
+  };
 
 
-Post.fetchAll = function(callback){
-    /*** Get eTag to see if the file on the server was changed ***/
-    /*** This helps to make sure the data in local storage is up to date ***/
-  $.ajax({
-    method: 'HEAD',
-    url: '/scripts/projectData.json',
-    success: function(data, message, xhr){
-      var newETag = xhr.getResponseHeader('ETag');
-      var oldETag = localStorage.eTag;
-      //console.log('old etag ' + oldETag);
-      //console.log('new etag ' + newETag);
-      if (oldETag == newETag) {
-        //file wasn't modified so use cached version
-        //console.log('taking data from local storage');
-        Post.loadAll(JSON.parse(localStorage.sourceData));
-        callback();
-      }
-      else {
-        //the file on the server was modified or this is the first time loading the site
-        localStorage.setItem('eTag', newETag);
-        $.ajax('/scripts/projectData.json').done(function(returnedObj){
-          //console.log('pulling a new file from the server');
-          localStorage.setItem('sourceData', JSON.stringify(returnedObj));
-          Post.loadAll(returnedObj);
+  Post.fetchAll = function(callback){
+      /*** Get eTag to see if the file on the server was changed ***/
+      /*** This helps to make sure the data in local storage is up to date ***/
+    $.ajax({
+      method: 'HEAD',
+      url: '/scripts/projectData.json',
+      success: function(data, message, xhr){
+        var newETag = xhr.getResponseHeader('ETag');
+        var oldETag = localStorage.eTag;
+        //console.log('old etag ' + oldETag);
+        //console.log('new etag ' + newETag);
+        if (oldETag == newETag) {
+          //file wasn't modified so use cached version
+          //console.log('taking data from local storage');
+          Post.loadAll(JSON.parse(localStorage.sourceData));
           callback();
-        });
+        }
+        else {
+          //the file on the server was modified or this is the first time loading the site
+          localStorage.setItem('eTag', newETag);
+          $.ajax('/scripts/projectData.json').done(function(returnedObj){
+            //console.log('pulling a new file from the server');
+            localStorage.setItem('sourceData', JSON.stringify(returnedObj));
+            Post.loadAll(returnedObj);
+            callback();
+          });
+        }
       }
-    }
-  });
-};
+    });
+  };
+  module.Post = Post;
+})(window);
--- scripts/projectView.js
+++ scripts/projectView.js
@@ -1,88 +1,91 @@
-var projectView = {};
+(function(module) {
+  var projectView = {};
 
-projectView.handleNavTabs = function(){
-  $('.tab').hide();
-  $('#main-nav').on('click', '.tabLink', function(event){
-    event.preventDefault();
-    var clickedLink = event.currentTarget;
-    var tabId = $(this).data('tab');
-    /*** Show/hide the correct tab ***/
-    $('#'+ tabId).show();
-    $('#'+ tabId).show().siblings().hide();
-    /*** Underline active tab***/
-    $('.tabLink').each(function(){
-      $(this).removeClass('active');
+  projectView.handleNavTabs = function(){
+    $('.tab').hide();
+    $('#main-nav').on('click', '.tabLink', function(event){
+      event.preventDefault();
+      var clickedLink = event.currentTarget;
+      var tabId = $(this).data('tab');
+      /*** Show/hide the correct tab ***/
+      $('#'+ tabId).show();
+      $('#'+ tabId).show().siblings().hide();
+      /*** Underline active tab***/
+      $('.tabLink').each(function(){
+        $(this).removeClass('active');
+      });
+      $(clickedLink).addClass('active');
     });
-    $(clickedLink).addClass('active');
-  });
-};
-
-projectView.populateFilters = function(){
-  /*** Populate filters using a Handlebars template ***/
-  var source = $('#filter-template').html();
-  var filterTemplate = Handlebars.compile(source);
-
-  var content = {
-    filter: [ ]
   };
 
-  /***Scan through categories in projectData and populate the drop down choices***/
-  $('article').each(function(){
-    var value = $(this).data('category');
-    /*** Checking to see if the value already exists in the array ***/
-    if (content.filter.indexOf(value) == -1) {
-      content.filter.push(value);
-    }
-  });
+  projectView.populateFilters = function(){
+    /*** Populate filters using a Handlebars template ***/
+    var source = $('#filter-template').html();
+    var filterTemplate = Handlebars.compile(source);
 
-  var compiledHtml = filterTemplate(content);
-  $('#category-filter').append(compiledHtml);
-  /*** End handlebars code ***/
+    var content = {
+      filter: [ ]
+    };
 
-};
-
-projectView.handleCategoryFilter = function(){
-  $('#category-filter').on('change', function(){
-    var selectedOption = $(this).val();
-    if(selectedOption){
-      $('article').hide().each(function(){
-        if($(this).data('category') == selectedOption){
-          $(this).show();
-        }
-      });
-    }
-    else {
-      /***Show all posts if the first item in the dropdown is selected***/
-      $('article').show();
-    }
-  });
-};
-projectView.setPreview = function(){
-  /*** Display only the first paragraph in the post body ***/
-  $('.post_body *:nth-of-type(n+2)').hide();
-  /***add event handler to display the full post body on click here***/
-  $('article').on('click', '.read_more ', function(event){
-    var clickedLink = $(this);
-    event.preventDefault();
-    $(this).parent().find('.post_body *:nth-of-type(n+2)').slideToggle(function(){
-      if($(this).is(':visible')){
-        clickedLink.html('<a>Read Less <i class="fa fa-arrow-circle-up"></i></a>');
-      }
-      else {
-        clickedLink.html('<a>Read More <i class="fa fa-arrow-circle-down"></i></a>');
+    /***Scan through categories in projectData and populate the drop down choices***/
+    $('article').each(function(){
+      var value = $(this).data('category');
+      /*** Checking to see if the value already exists in the array ***/
+      if (content.filter.indexOf(value) == -1) {
+        content.filter.push(value);
       }
     });
-  });
-};
 
-projectView.initHomePage = function(){
-  /*** Writes blog posts to the page and enables views ***/
-  Post.all.forEach(function(content){
-    $('#articles').append(content.toHtml());
-  });
+    var compiledHtml = filterTemplate(content);
+    $('#category-filter').append(compiledHtml);
+    /*** End handlebars code ***/
 
-  projectView.handleNavTabs();
-  projectView.populateFilters();
-  projectView.handleCategoryFilter();
-  projectView.setPreview();
-};
+  };
+
+  projectView.handleCategoryFilter = function(){
+    $('#category-filter').on('change', function(){
+      var selectedOption = $(this).val();
+      if(selectedOption){
+        $('article').hide().each(function(){
+          if($(this).data('category') == selectedOption){
+            $(this).show();
+          }
+        });
+      }
+      else {
+        /***Show all posts if the first item in the dropdown is selected***/
+        $('article').show();
+      }
+    });
+  };
+  projectView.setPreview = function(){
+    /*** Display only the first paragraph in the post body ***/
+    $('.post_body *:nth-of-type(n+2)').hide();
+    /***add event handler to display the full post body on click here***/
+    $('article').on('click', '.read_more ', function(event){
+      var clickedLink = $(this);
+      event.preventDefault();
+      $(this).parent().find('.post_body *:nth-of-type(n+2)').slideToggle(function(){
+        if($(this).is(':visible')){
+          clickedLink.html('<a>Read Less <i class="fa fa-arrow-circle-up"></i></a>');
+        }
+        else {
+          clickedLink.html('<a>Read More <i class="fa fa-arrow-circle-down"></i></a>');
+        }
+      });
+    });
+  };
+
+  projectView.initHomePage = function(){
+    /*** Writes blog posts to the page and enables views ***/
+    Post.all.forEach(function(content){
+      $('#articles').append(content.toHtml());
+    });
+
+    projectView.handleNavTabs();
+    projectView.populateFilters();
+    projectView.handleCategoryFilter();
+    projectView.setPreview();
+  };
+  module.projectView = projectView;
+})(window);
