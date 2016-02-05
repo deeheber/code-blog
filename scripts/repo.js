eaa8b690fed8d3dd62726efd8714fe05a5c49f04
Danielle H <daheber@gmail.com>
2016-02-04 21:20:49 -0800

    gitignore rules and ajax call to load repos

---
+++ scripts/repo.js
@@ -0,0 +1,18 @@
+(function(module){
+  var repos = {};
+
+  repos.all = [];
+
+  repos.fetchAll = function(callback){
+    $.ajax({
+      url: 'https://api.github.com/users/deeheber/repos',
+      type: 'GET',
+      headers: {'Authorization': 'token ' + githubToken },
+      success: function(data, message, xhr){
+        console.log(data);
+        repos.all = data;
+      }
+    }).done(callback);
+  };
+  module.repos = repos;
+})(window);
