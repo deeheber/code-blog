41b1abaa378c41a28079af12cac4e5563586c33b
Danielle H <daheber@gmail.com>
2016-02-06 12:09:26 -0800

    sorts repos by last git push

--- scripts/repo.js
+++ scripts/repo.js
@@ -3,6 +3,13 @@
 
   repos.all = [];
 
+  /*** Sorts repos by the last git push date ***/
+  repos.loadAll = function(data){
+    data.sort(function(a,b) {
+      return (new Date(b.pushed_at)) - (new Date(a.pushed_at));
+    });
+  };
+
   repos.fetchAll = function(callback){
     $.ajax({
       url: 'https://api.github.com/users/deeheber/repos',
@@ -12,7 +19,10 @@
         //console.log(data);
         repos.all = data;
       }
-    }).done(callback);
+    }).done(function(){
+      repos.loadAll(repos.all);
+      callback();
+    });
   };
   module.repos = repos;
 })(window);
