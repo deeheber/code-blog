fed3fe99a8b0abbfe039de039043266bc946a11d
Danielle H <daheber@gmail.com>
2016-02-11 22:06:56 -0800

    verified controller functions fire

--- controller/homeControllers.js
+++ controller/homeControllers.js
@@ -4,22 +4,25 @@
   topNav.handleNav();
 
   homeController.index = function(){
-    /*** Load Article Data into HTML ***/
-    Post.fetchAll(projectView.initHomePage);
-    /*** view for the page ***/
-    $('main > div').hide();
-    $('#home').show();
-    /*** If href = / then make that nav link .active ***/
-    $('a').removeClass('active');
-    $('a[href$="/"]').addClass('active');
+    // /*** Load Article Data into HTML ***/
+    // Post.fetchAll(projectView.initHomePage);
+    // /*** view for the page ***/
+    // $('main > div').hide();
+    // $('#home').show();
+    // /*** If href = / then make that nav link .active ***/
+    // $('a').removeClass('active');
+    // $('a[href$="/"]').addClass('active');
+    console.log('index loaded');
   };
 
   homeController.loadAll = function(ctx, next){
-    //next();
+    console.log('load all works');
+    next();
   };
 
   homeController.loadByCategory = function(ctx, next){
-    //next();
+    console.log('load by category works');
+    next();
   };
 
   module.homeController = homeController;
