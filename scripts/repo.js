39aaf36675ab4a7d07d2e759ffa9cce83db86a3b
Danielle H <daheber@gmail.com>
2016-02-11 23:30:46 -0800

    Refactored ajax call to GitHub API

--- scripts/repo.js
+++ scripts/repo.js
@@ -17,17 +17,20 @@
       type: 'GET',
       success: function(data, message, xhr){
         repos.all = data;
+        /*** Filter out repositories that are forks of someone else's***/
+        var excludeForkRepos = repos.all.filter(function(repository){
+          return repository.fork === false;
+        });
+        /*** Sort by most recent push to the repo ***/
+        excludeForkRepos.sort(function(a,b) {
+          return (new Date(b.pushed_at)) - (new Date(a.pushed_at));
+        });
+        callback(excludeForkRepos);
+      },
+      fail: function(xhr, status, errorThrown){
+        alert('Sorry, the server is unavailable. Please try again later.');
+        callback();
       }
-    }).done(function(){
-      /*** Filter out repositories that are forks of someone else's***/
-      var excludeForkRepos = repos.all.filter(function(repository){
-        return repository.fork === false;
-      });
-      /*** Sort by most recent push to the repo ***/
-      excludeForkRepos.sort(function(a,b) {
-        return (new Date(b.pushed_at)) - (new Date(a.pushed_at));
-      });
-      callback(excludeForkRepos);
     });
   };
   module.repos = repos;
