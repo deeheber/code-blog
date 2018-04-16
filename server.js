bcded96332617aaf35f5144715f9cb87bfce2eec
Danielle H <daheber@gmail.com>
2018-04-16 11:37:42 -0700

    remove server side console.log

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ var requestProxy = require('express-request-proxy'),
 
 var proxyGitHub = function(request, response) {
   console.log('Routing GitHub request for', request.params[0]);
-  console.log('GITHUBTOKEN IS '+ process.env.GITHUB_TOKEN);
   (requestProxy({
     url: 'https://api.github.com/' + request.params[0],
     headers: { 'Authorization': 'token ' + process.env.GITHUB_TOKEN },
